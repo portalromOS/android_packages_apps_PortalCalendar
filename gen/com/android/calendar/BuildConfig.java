4df3534d65dc1cfb9d6e46212ed1b0f9fdeacb64
PortalRomOS <portalromOS@gmail.com>
2023-03-08 11:32:02 +0000

    Change folder layout to project original and changed android.bp

--- gen/com/android/calendar/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-
-package com.portal.calendar;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = false;
-    public static final String APPLICATION_ID = "com.portal.calendar";
-}
